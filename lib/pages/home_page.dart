68ad283365b181be131f02b2938ad7126a044330
khushboo jain <khushiejain92@gmail.com>
2024-06-18 17:29:54 +0530

    getting started with to do tasks

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:taskbunny/util/todo_tile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,22 +9,38 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // List of todo tasks
+  List toDoList = [
+    ["bake a cake", false],
+    ["Do Exercise", false],
+  ];
+
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurple[100],
       appBar: AppBar(
-        title : Text("TaskBunny"),
-        backgroundColor:Colors.deepPurple[200],
+        title: const Text("TaskBunny"),
+        backgroundColor: Colors.deepPurple[200],
         elevation: 0,
         centerTitle: true,
       ),
-      /*body: ListView(
-        children: [
-          ToDoList(),
-        ],
-      ),*/
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return ToDoTile(
+            taskName: toDoList[index][0], // Corrected to use the right index
+            taskCompleted: toDoList[index][1],
+            onChanged: (value) => checkBoxChanged(value, index), // Corrected callback
+          );
+        },
+      ),
     );
   }
 }
--- lib/util/todo_tile.dart
+++ lib/util/todo_tile.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/material.dart';
 
 class ToDoTile extends StatelessWidget {
-  const ToDoTile({super.key});
-
+  final String taskName;
+  final bool taskCompleted;
+  Function(bool?)? onChanged;
+  
+  ToDoTile({
+  super.key,
+  required this.taskName, 
+  required this.taskCompleted,
+  required this.onChanged,
+  });
+ 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    ); 
+    return Padding(
+      padding: const EdgeInsets.only(left: 25.0,right: 25, top:25),
+      child: Container(
+        padding: EdgeInsets.all(28),
+        child: Row
+        (children: [
+          //checkbox
+          Checkbox(value: taskCompleted, onChanged: onChanged),
+          //taskname
+          Text(taskName),
+        ],
+        ),
+        decoration: BoxDecoration(
+          color: Colors.deepPurple[50],
+          borderRadius: BorderRadius.circular(12),
+          ),
+      ),
+    );
   }
 }
